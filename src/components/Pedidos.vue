4e2c6b18f1e77e1a7d4f381579b28db20cd0ae97
Vitor Davi Pereira Soares <79873418+vitordavips@users.noreply.github.com>
2024-01-17 14:46:48 -0400

    criando o Dashboard

---
+++ src/components/Pedidos.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="main-container">
+        <h1>Gerenciar pedidos</h1>
+        <p>dashboard</p>
+    </div>    
+</template>
+
+<script>
+export default {
+    name: "Pedidos",
+}
+</script>
