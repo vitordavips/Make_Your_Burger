1dfa434e74ff467b4e110b70a8165ff8919da80a
Vitor Davi Pereira Soares <79873418+vitordavips@users.noreply.github.com>
2024-01-21 01:50:53 -0400

    criando o DELETE

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -32,7 +32,7 @@
                         {{ s.tipo }}
                     </option>
                 </select>
-                <button class="delete-btn">Cancelar</button>
+                <button class="delete-btn" @click="deleteBurger(burger.id)">Cancelar</button>
             </div>
         </div>
     </div>
@@ -69,6 +69,17 @@ export default {
             this.status = data;
 
             
+        },
+        async deleteBurger(id){
+            const req = await fetch(`http://localhost:3000/burgers/${id}`, {
+                method: 'DELETE'
+            });
+
+            const res = await req.json();
+
+            //msg
+
+            this.getPedidos();
         }
     },
     mounted(){
